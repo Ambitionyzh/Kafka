7f25ca9f832532d8904f15f5ea69f2b139542ee1
wuhuqifei <1214086027@qq.com>
2023-04-28 21:07:34 +0800

    v1

--- src/main/java/com/yongzh/kafka/producer/CustomProducer.java
+++ src/main/java/com/yongzh/kafka/producer/CustomProducer.java
@@ -20,7 +20,7 @@ public class CustomProducer {
         Properties properties = new Properties();
 
         //连接集群 bootstrap.servers
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop102:9002,hadoop103:9002");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop102:9092,hadoop103:9092");
 
         //指定对应key和value的序列化类型
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
